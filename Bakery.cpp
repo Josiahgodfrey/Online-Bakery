7d0de4d01e9156aab7cfb939fbfeb118fb777356
Josiah Godfrey <johgr2585@gmail.com>
2025-01-02 15:04:27 +0300

    third

--- Bakery.cpp
+++ Bakery.cpp
@@ -7,3 +7,16 @@ using namespace std;
 #include <sstream>
 #include <iomanip>
 #include <openssl/sha.h>
+
+class Bakery {
+protected:
+    int water;
+    int flour;
+    int oil;
+
+public:
+    Bakery(int water, int flour, int oil)
+        : water(water), flour(flour), oil(oil) {}
+
+    virtual void commonIngredient() const = 0; // Corrected method name
+};
